5ccc212e91cf86804d3e1bba861f1be16b97ab66
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-12-15 18:50:14 +0530

    Using Classes

---
+++ src/classes.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+class Classes:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.printButton = Button(frame, text="Print message", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quitButton = Button(frame, text="Quit", command=frame.quit)
+        self.quitButton.pack(side=LEFT)
+
+    def printMessage(self):
+        print("Well this actually worked!")
+
+root = Tk()
+b = Classes(root)
+root.mainloop()
